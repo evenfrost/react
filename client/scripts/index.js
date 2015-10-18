b950ab93b3461f30451d0a803aaa7a97c9dc425f
evenfrost <kisolov@gmail.com>
2015-10-19 01:15:21 +0300

    Update stuff

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
+/**
+ * Components.
+ */
 let ContactItem = React.createClass({
   propTypes: {
     name: React.PropTypes.string.isRequired,
@@ -21,44 +24,54 @@ let ContactItem = React.createClass({
 let ContactForm = React.createClass({
   propTypes: {
     contact: React.PropTypes.object.isRequired,
-    onChange: React.PropTypes.func.isRequired
+    onChange: React.PropTypes.func.isRequired,
+    onSubmit: React.PropTypes.func.isRequired
+  },
+
+  onNameChange: function (event) {
+    this.props.onChange(Object.assign({}, this.props.value, {name: event.target.value}));
+  },
+  
+  onEmailChange: function (event) {
+    this.props.onChange(Object.assign({}, this.props.value, {email: event.target.value}));
+  },
+  
+  onDescriptionChange: function (event) {
+    this.props.onChange(Object.assign({}, this.props.value, {description: event.target.value}));
+  },
+
+  onSubmit: function (event) {
+    event.preventDefault();
+    this.props.onSubmit();
   },
 
   render: function () {
-    let contact = this.props.contact;
-    let onChange = this.props.onChange;
+    let errors = this.props.contact.errors || {};
 
     return React.createElement('form', {
-        onSubmit: event => {
-          let contact = this.props.contact;
-
-          event.preventDefault();
-
-          if (contact.name && contact.email) {
-            contacts.push(Object.assign(contact, { key: currentKey++ }));
-            return render(contact, contacts);
-          }
-        }
+        onSubmit: this.onSubmit
       },
       React.createElement('input', {
         type: 'text',
         name: 'name',
         placeholder: 'Name',
         value: this.props.contact.name,
-        onChange: event => onChange(Object.assign({}, contact, { name: event.target.value }))
+        className: errors.name && 'error',
+        onChange: this.onNameChange 
       }),
       React.createElement('input', {
         type: 'email',
         name: 'email',
         placeholder: 'Email',
         value: this.props.contact.email,
-        onChange: event => onChange(Object.assign({}, contact, { email: event.target.value }))
+        className: errors.email && 'error',
+        onChange: this.onEmailChange
       }),
       React.createElement('textarea', {
         name: 'description',
         placeholder: 'Description',
         value: this.props.contact.description,
-        onChange: event => onChange(Object.assign({}, contact, { description: event.target.value }))
+        onChange: this.onDescriptionChange
       }),
       React.createElement('button', { type: 'submit' }, 'Add Contact')
     );
@@ -69,52 +82,70 @@ let ContactView = React.createClass({
   propTypes: {
     contacts: React.PropTypes.array.isRequired,
     newContact: React.PropTypes.object.isRequired,
-    onNewContactChange: React.PropTypes.func.isRequired
+    onNewContactChange: React.PropTypes.func.isRequired,
+    onNewContactSubmit: React.PropTypes.func.isRequired
   },
 
   render: function () {
     return React.createElement('div', {}, 
       React.createElement('h1', {}, 'Contacts'),
       React.createElement('ul', {},
-        this.props.contacts.map(contact => React.createElement(ContactItem, contact))
+        this.props.contacts
+          .filter(contact => contact.email)
+          .map(contact => React.createElement(ContactItem, contact))
       ),
       React.createElement(ContactForm, {
         contact: this.props.newContact,
-        onChange: this.props.onNewContactChange
+        onChange: this.props.onNewContactChange,
+        onSubmit: this.props.onNewContactSubmit
       })
     );
     
   }
 });
 
-let currentKey = 0;
+const CONTACT = { name: '', email: '', description: '', errors: null };
 
-let contacts = [{ 
-  key: currentKey++,
-  name: 'John Doe',
-  email: 'john@doe.com',
-  description: 'Front-end Unicorn'
-}, {
-  key: currentKey++,
-  name: 'John Smith',
-  email: 'john@smith.com'
-}];
+let updateNewContact = newContact => setState({ newContact });
 
-let newContact = {
-  name: 'Aleksey',
-  email: '',
-  description: ''
-};
- 
+let submitNewContact = () => {
+  let contact = Object.assign({}, state.newContact, { key: state.contacts.length + 1, errors: {} });
+  
+  if (!contact.name) {
+    contact.errors.name = ["Please enter your new contact's name"];
+  }
+  if (!/.+@.+\..+/.test(contact.email)) {
+    contact.errors.email = ["Please enter your new contact's email"];
+  }
 
-let render = (contact, contacts) => {
-  ReactDOM.render(
-    React.createElement(ContactView, {
-      contacts: contacts,
-      newContact: contact,
-      onNewContactChange: contact => render(contact, contacts)
-    }), document.querySelector('#app')
+  setState(
+    Object.keys(contact.errors).length === 0
+    ? {
+        newContact: Object.assign({}, CONTACT),
+        contacts: state.contacts.slice(0).concat(contact)
+      }
+    : { newContact: contact }
   );
 };
 
-render(newContact, contacts);
+let state = {};
+
+let setState = changes => {
+  Object.assign(state, changes);
+  
+  ReactDOM.render(
+    React.createElement(ContactView, Object.assign({}, state, {
+      onNewContactChange: updateNewContact,
+      onNewContactSubmit: submitNewContact
+    })),
+    document.getElementById('app')
+  );
+};
+
+setState({
+  contacts: [
+    {key: 1, name: 'James K Nelson', email: 'james@jamesknelson.com', description: 'Front-end Unicorn'},
+    {key: 2, name: 'Jim', email: 'jim@example.com'}
+  ],
+  newContact: Object.assign({}, CONTACT)
+});
