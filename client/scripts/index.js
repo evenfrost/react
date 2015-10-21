ffdbfe76387616ad3db9e1a41883e958482804b0
evenfrost <evenfrosty@gmail.com>
2015-10-21 18:55:55 +0300

    Update stuff

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,11 +5,65 @@ let mountNode = document.getElementById('app');
 
 let Countdown = React.createClass({
 
+  componentDidMount: function () {
+    this.interval = setInterval(this.props.tick, 1000);
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this.interval);
+  },
+
   render: function () {
-    return {
-      div
-    }
-    
+    return (
+      <div>{ this.props.secondsToGo }</div>
+    );
   }
 
 });
+
+let CountdownForm = React.createClass({
+  updateCounter: function (event) {
+    event.preventDefault();
+
+    let form = event.target;
+    let amount = form.amount.value;
+
+    this.props.updateCounter(amount);
+  },
+
+  render: function () {
+    return (
+      <form onSubmit={ this.updateCounter }>
+        <input type="number" name="amount" />
+        <button type="submit">Update</button>
+      </form>
+    );
+  }
+
+});
+
+let CountdownView = React.createClass({
+  getInitialState: function () {
+    return { secondsToGo: 500 };
+  },
+
+  updateCounter: function (amount) {
+    amount = +amount || 0;
+    this.setState({ secondsToGo: amount });
+  },
+
+  tick: function () {
+    this.setState({ secondsToGo: this.state.secondsToGo - 1 });
+  },
+
+  render: function () {
+    return (
+      <div>
+        <Countdown secondsToGo={ this.state.secondsToGo } tick={ this.tick } />
+        <CountdownForm updateCounter={ this.updateCounter } />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<CountdownView />, mountNode);
